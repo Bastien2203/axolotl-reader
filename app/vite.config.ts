eec9933d9f55dc17c6756b71ac0e406ef870231b
bastien.grisvard <bastien.grisvard@raccourci.fr>
2025-05-06 17:07:34 +0200

    feat: add PWA support with vite-plugin-pwa and update package.json

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.png'], 
+      manifest: {
+        name: 'Axolotl Reader',
+        short_name: 'Axolotl Reader',
+        description: 'Comics and Manga Reader',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
