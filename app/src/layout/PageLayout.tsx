26b779b39878f2ed5893de1212f0ba420d116f5e
bastien.grisvard <bastien.grisvard@raccourci.fr>
2025-05-13 13:18:28 +0200

    fix: remove unnecessary padding top class from PageLayout component

--- app/src/layout/PageLayout.tsx
+++ app/src/layout/PageLayout.tsx
@@ -8,7 +8,7 @@ type PageLayoutProps = {
 
 const PageLayout = (props: PropsWithChildren<PageLayoutProps>) => (
     <>
-        <div className="bg-base-300 p-4 flex items-center gap-4 fixed z-10 w-full pt-safe">
+        <div className="bg-base-300 p-4 flex items-center gap-4 fixed z-10 w-full">
             {
                 props.onBack && <button
                     onClick={props.onBack}
